64d592166836d2c388b901e91e012905c922ec3d
Grimlerih <61974670+Grimlerih@users.noreply.github.com>
2022-12-06 19:01:46 +0300

    feat: Слушатель на кнопку esc

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 import styles from './Modal.module.css'
 import {ModalOverlay} from '../ModalOverlay/ModalOverlay'
 import {CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import {createPortal} from 'react-dom'
 
 export const Modal = ({setOpenModal, children}) => {
-    const closeModal = () => {
+
+    const handlerCloseModal = () => {
         setOpenModal(false);
     }
 
+    useEffect (() => {
+        const buttonCloseModal = (event) => {
+            if (event.key === "Escape") {
+                handlerCloseModal();
+            }
+        }
+        window.addEventListener("keydown", buttonCloseModal);
+        return () => window.removeEventListener("keydown", buttonCloseModal);
+    }, [])
+
   return createPortal(
     <>
-        <ModalOverlay closeModalOverlay={closeModal}/>
+        <ModalOverlay closeModalOverlay={handlerCloseModal}/>
         <div className={styles.container}>
-        <button className={styles.closeButton}>
-          <CloseIcon type="primary" />
-        </button>
+          <button className={styles.closeButton} onClick={handlerCloseModal}>
+            <CloseIcon type="primary" />
+          </button>
         {children}
         </div>
     </>, document.querySelector("#modal")
